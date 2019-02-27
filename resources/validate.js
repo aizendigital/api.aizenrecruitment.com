b18e5b06f71ba5fabe139a390b0ea9d4fbd10edf
Idin Khayami <idin.khanoom.khayami@gmail.com>
2019-02-27 20:59:02 +0330

    Add company name

--- resources/validate.js
+++ resources/validate.js
@@ -6,6 +6,7 @@ module.exports = function  validate(user){
     const schema = {
         name: joi.string().min(2).max(128).required(),
         email: joi.string().required().email().max(128),
+        company: joi.string().min(0),
         positionhire: joi.string().min(0),
         positiontype: joi.string().min(0),
         phone: joi.number(),
