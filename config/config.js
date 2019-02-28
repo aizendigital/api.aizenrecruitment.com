718e5cde2623ae6c8c98dbe2a2afbc3d3aa03771
Idin Khayami <idin.khanoom.khayami@gmail.com>
2019-02-28 10:41:36 +0330

    fix email

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@ const config = {
       mail:{
         email_user: process.env.EMAIL_USER || 'testiit775@gmail.com',
         email_password: process.env.EMAIL_PASSWORD || 'Am123123Am123123',
-        email_to: process.env.EMAIL_TO || ('idin.khanoom.khayami@gmail.com' && 'idin.khanoom.khayami@gmail.com')
+        email_to: process.env.EMAIL_TO || ('idin.khanoom.khayami@gmail.com' && 'mdh.heydari@gmail.com')
       }
 }
 
